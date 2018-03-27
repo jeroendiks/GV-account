5b87e5e84f8ed892c410fe91d4f73a50e8aec529
Jeroen Diks <jeroen.diks@incentro.com>
2018-03-27 10:44:47 +0200

    Improved overall

--- src/account/create-account/create-account.js
+++ src/account/create-account/create-account.js
@@ -2,16 +2,13 @@ import React from 'react';
 import '../../account/account.css';
 
 const CreateAccount = () => {
-    const createAccount = event => (console.log('Account created!'));
-
     return (
         <form className="create-account-box">
             <h3>Create Account</h3>
             <p className="create-account-box__text"><span>&#10003;</span>All orders and returns in one place</p>
             <p className="create-account-box__text"><span>&#10003;</span>Your shopping cart everywhere</p>
             <p className="create-account-box__text"><span>&#10003;</span>Create and save your wishlist</p>
-            <button onClick={createAccount}
-                    className="button button--secondary create-account-box__button">
+            <button className="button button--secondary create-account-box__button">
                 <a href="#">Create Account</a>
             </button>
         </form>
